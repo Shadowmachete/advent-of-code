4b22d47ad1e9e5a241f2e0e7e807c6eb629949e0
Shadowmachete <vijayramanathan9@gmail.com>
2025-12-02 19:06:42 +0800

    bump

--- aoc_tools/consts.py
+++
@@ -1,6 +0,0 @@
-"""
-Common constants used in Advent of Code problems.
-"""
-
-DIRS4 = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-DIRS8 = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
